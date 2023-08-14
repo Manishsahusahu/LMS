d2b27f87f1839cffe880f894b17351887a7d3ef2
Manishsahusahu <sahumanish327@gmail.com>
2023-08-14 18:39:37 +0530

    razorpay payment verification

--- server/controllers/payment.controller.js
+++ server/controllers/payment.controller.js
@@ -1,5 +1,7 @@
 import AppError from "../utils/error.util.js";
 import User from "../models/user.model.js";
+import Payment from "../models/payment.model.js";
+import crypto from "crypto";
 
 const getRazorpayApiKey = async (req, res, next) => {
   res.status(200).json({
@@ -8,6 +10,7 @@ const getRazorpayApiKey = async (req, res, next) => {
     key: process.env.RAZORPAY_KEY_ID,
   });
 };
+
 const buySubscription = async (req, res, next) => {
   const { id } = req.user;
 
@@ -34,8 +37,37 @@ const buySubscription = async (req, res, next) => {
     subscription_id: subscription.id,
   });
 };
-const verifySubscription = async (req, res, next) => {};
+
+const verifySubscription = async (req, res, next) => {
+  const { id } = req.user;
+  const { razorpay_payment_id, razorpay_signature, razorpay_subscription_id } =
+    req.body;
+
+  const user = await User.findById(id);
+  if (!user) return next(new AppError("Kindly login", 400));
+
+  const userSubscriptionId = user.subscription.id;
+  const generatedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_SCRET)
+    .update(`${razorpay_payment_id}|${userSubscriptionId}`)
+    .digest("hex");
+
+  if (generatedSignature !== razorpay_signature)
+    return next(new AppError("Payment is not verified, Please try again", 500));
+
+  const payment = await Payment.create(req.body);
+
+  user.subscription.status = "active";
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Payment verification successfull",
+  });
+};
+
 const cancelSubscription = async (req, res, next) => {};
+
 const allPayments = async (req, res, next) => {};
 
 export {
--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export const razorpay= new Razorpay({
+const razorpay= new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_SECRET
 })
